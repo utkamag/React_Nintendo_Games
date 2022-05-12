607cbfc60cc000ffb25d6eeef95c13782aab3140
utkamag <92789705+utkamag@users.noreply.github.com>
2022-05-12 20:28:50 +0400

    Update code

--- react-app/src/components/Data.tsx
+++ react-app/src/components/Data.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Card, Container, Grid, Typography} from "@mui/material";
+import {Box, Card, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
 
 interface apiProps {
     data: any;
@@ -14,9 +14,11 @@ const NintendoList: React.FC<apiProps> = ({data}) => {
                         <Grid container spacing={2} key={dat.id}>
                             <Grid item md={12} sx={{marginTop: "20px"}}>
                                 <Card sx={{padding: "10px"}}>
+                                    <CardContent>
                                     <Typography variant="h5" sx={{padding: "10px"}}>{dat.name}</Typography>
                                     <Typography>Разработчик: {dat.developers}</Typography>
                                     <Typography>Дата релиза в Японии: {dat.releaseDates.Japan}</Typography>
+                                    </CardContent>
                                 </Card>
                             </Grid>
                         </Grid>
